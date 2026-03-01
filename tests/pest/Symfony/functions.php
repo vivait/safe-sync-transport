5187dd658a659cbfad74ea9ec94a84f57158936f
Kiel Goodman <kieljohn@users.noreply.github.com>
2026-03-01 16:02:50 +0000

    Too much of a pain to get cross support, so version bump

--- tests/pest/Symfony/functions.php
+++ tests/pest/Symfony/functions.php
@@ -6,7 +6,6 @@ namespace Pest\Custom\Symfony;
 
 use LogicException;
 use Pest\Exceptions\ShouldNotHappen;
-use PHPUnit\Runner\Version;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -16,25 +15,14 @@ function app(bool $reInstanciate = false): KernelInterface
     static $kernel;
 
     if (null === $kernel || $reInstanciate) {
-        if (version_compare(Version::id(), '11.0.0', '>=')) {
-            $testCase = new class ('kernel') extends KernelTestCase {
-                public function getKernel(): KernelInterface
-                {
-                    self::bootKernel();
+        $testCase = new class ('kernel') extends KernelTestCase {
+            public function getKernel(): KernelInterface
+            {
+                self::bootKernel();
 
-                    return self::$kernel;
-                }
-            };
-        } else {
-            $testCase = new class () extends KernelTestCase {
-                public function getKernel(): KernelInterface
-                {
-                    self::bootKernel();
-
-                    return self::$kernel;
-                }
-            };
-        }
+                return self::$kernel;
+            }
+        };
         $kernel = $testCase->getKernel();
     }
 
